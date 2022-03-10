3ff53ead8033d64bf2a2843d1814a2b5a4797472
Jens Weber <jens@uvic.ca>
2022-03-10 00:43:10 -0800

    bolt project file

--- project.clj
+++ project.clj
@@ -3,19 +3,21 @@
   :url "http://jenshweber.github.io/grape/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [org.clojure/data.json "0.2.6"]
                  [dorothy "0.0.6"]
                  [org.clojure/data.codec "0.1.0"]
                  [prismatic/schema "1.0.5"]
-                 [clojurewerkz/neocons "3.1.0"]
+                 [clojurewerkz/neocons "3.2.0"]
                  [com.taoensso/timbre "4.3.1"]
                  [environ "1.0.2"]
                  [nano-id "0.9.3"]
                  [proto-repl "0.3.1"]
                  [gorilla-graph "0.1.0-SNAPSHOT"]
-                 [org.clojars.benfb/gorilla-repl "0.6.0"]]
+                 [org.clojars.benfb/gorilla-repl "0.6.0"]
+                 [gorillalabs/neo4j-clj "4.1.2"]
+                 [org.neo4j.driver/neo4j-java-driver "4.1.1"]]
   :plugins [[lein-environ "1.1.0"]
             [org.clojars.benfb/lein-gorilla "0.6.0"]]
   :profiles {:dev {}}
